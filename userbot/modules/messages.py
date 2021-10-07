206a7a893484ef85d7c0efe585ab6e7c40d831a6
Ridwansyah Boman <86453476+Bomansyah@users.noreply.github.com>
2021-10-08 03:41:45 +0700

    Update messages.py

--- userbot/modules/messages.py
+++ userbot/modules/messages.py
@@ -47,7 +47,7 @@ async def fastpurger(purg):
 
 
 @register(outgoing=True, pattern=r"^\.del")
-async def purgeme(delme):
+async def del(delme):
     message = delme.text
     count = int(message[9:])
     i = 1
