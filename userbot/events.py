290a58a42d3be8253523ea4e0e3ff2845d4cf5b9
Ridwansyah Boman <86453476+Bomansyah@users.noreply.github.com>
2021-10-10 00:39:15 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -186,7 +186,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ MAN-USERBOT ERROR REPORT ✘**\n\n"
+                    text = "**🇮🇩TORNADO-USERBOT ERROR REPORT 🇮🇩**\n\n"
                     link = "[Group Support](https://t.me/SharingUserbot)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
@@ -196,7 +196,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @SharingUserbot"
+                    ftext += "\ncukup forward saja pesan ini ke @TornadoComunity"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
