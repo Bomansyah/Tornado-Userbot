00db2ce1da7379c978785f1ea50b71fad7f65e52
Ridwansyah Boman <86453476+Bomansyah@users.noreply.github.com>
2021-10-10 00:35:07 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,32 +5,53 @@
 #
 """ Userbot start point """
 
+import sys
 from importlib import import_module
-from sys import argv
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
+from telethon.tl.functions.channels import JoinChannelRequest
+
+from userbot import ALIVE_NAME, BOTLOG_CHATID, BOT_VER, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES
 
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n Tip: Use Country Code along with number.' \
-             '\n or check your phone number and try again !'
+INVALID_PH = (
+    "\nERROR: Nomor Telepon yang kamu masukkan SALAH."
+    "\nTips: Gunakan Kode Negara beserta nomornya atau periksa nomor telepon Anda dan coba lagi."
+)
 
 try:
     bot.start()
 except PhoneNumberInvalidError:
     print(INVALID_PH)
-    exit(1)
+    sys.exit(1)
 
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
+LOGS.info(
+    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Gabung ke Grup https://t.me/TornadoComunity")
 
 LOGS.info(
-    f"Tornado-Userbot ⚙️ V{BOT_VER} [🌀 BERHASIL DIAKTIFKAN! 🌀")
+    f"Tornado-Userbot ⚙️ V{BOT_VER} [『Tornado-Userbot Telah Aktif』]")
 
 
-if len(argv) not in (1, 3, 4):
+async def man_userbot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(BOTLOG_CHATID, f"🔥 **『Tornado-Userbot Telah Aktif』**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n➠ **Ketik** `.alive` **untuk Mengecheck Bot**\n━━")
+    except Exception as e:
+        LOGS.info(str(e))
+# KALO LU NGEFORK LINK CH & GRUP PUNYA GUA NYA JANGAN DI HAPUS YA GOBLOK 😡
+    try:
+        await bot(JoinChannelRequest("@GenshinHunter"))
+        await bot(JoinChannelRequest("@TornadoComunity"))
+    except BaseException:
+        pass
+# JANGAN DI HAPUS GOBLOK 😡 LU COPY/EDIT AJA TINGGAL TAMBAHIN PUNYA LU
+# DI HAPUS GUA GBAN YA 🥴 GUA TANDAIN LU AKUN TELENYA 😡
+bot.loop.create_task(man_userbot_on())
+
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
