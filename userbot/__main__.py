5fe7cddab966d21236e59d9f48a572865457c5bf
Ridwansyah Boman <86453476+Bomansyah@users.noreply.github.com>
2021-10-19 08:50:16 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -38,7 +38,7 @@ LOGS.info(
 async def man_userbot_on():
     try:
         if BOTLOG_CHATID != 0:
-            await bot.send_message(BOTLOG_CHATID, f"🔥 **Man-Userbot Berhasil Di Aktifkan**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n➠ **Ketik** `.alive` **untuk Mengecheck Bot**\n━━")
+            await bot.send_message(BOTLOG_CHATID, f"🔥 **『Nanggroe-Userbot Telah Aktif』**")
     except Exception as e:
         LOGS.info(str(e))
 # KALO LU NGEFORK LINK CH & GRUP PUNYA GUA NYA JANGAN DI HAPUS YA GOBLOK 😡
